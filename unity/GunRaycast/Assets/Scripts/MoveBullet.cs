ca86b706fbe470c106073537a770fc38dc6a971a
fabrice garnier <garnier06@gmail.com>
2016-01-22 18:00:19 +0100

    Add Recast_Gun

---
+++ unity/GunRaycast/Assets/Scripts/MoveBullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBullet : MonoBehaviour
+{
+
+		public float speed = 10f;
+		private float decremont = 1.5f;
+
+		void Start ()
+		{
+				Destroy (gameObject, 5f); //Delete the bullet after 5 seconds
+		}
+
+		void Update ()
+		{
+				transform.Translate (0, 0, speed);
+		}
+}
