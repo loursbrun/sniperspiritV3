e46a9673e787c117badedbf7b56faf9447be9a7e
fabrice garnier <garnier06@gmail.com>
2016-01-26 14:15:14 +0100

    Add window zoom + fix height person + add hit number (no class implemente ...)

---
+++ unity/GunRaycast/Assets/Scripts/InfoBullet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfoBullet : MonoBehaviour
+{
+
+		/*
+		* Public
+		*/
+
+		//public GUIText Bullet_X;
+		//public GUIText Bullet_Y;
+		//public GUIText Bullet_Z;
+
+		/*
+		* Private
+		*/
+
+		/*
+		* Constructor
+		*/
+
+		void Start ()
+		{
+			
+		}
+
+
+		void Update ()
+		{
+	
+		}
+}
